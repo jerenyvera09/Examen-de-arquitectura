2c30478773269bd9a543da3ab3d0827dd50e38a1
jeremy vera <hache23vera@gmail.com>
2025-06-14 01:18:50 -0500

    Fase 2 - errores corregidos en page

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,6 +9,9 @@ import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('Supabase Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
